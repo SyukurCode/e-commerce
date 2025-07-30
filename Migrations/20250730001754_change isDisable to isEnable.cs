62c095ef3db394c31053c83a7e1cbd0975077376
syukurcode <syukurz87@gmail.com>
2025-07-30 11:37:40 +0800

    add migration file

---
+++ Migrations/20250730001754_change isDisable to isEnable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace E_Commers_Adelia.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeisDisabletoisEnable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "isDisable",
+                table: "SellerPaymentMethods",
+                newName: "isEnable");
+
+            migrationBuilder.RenameColumn(
+                name: "isDisable",
+                table: "SellerDeliveryOptions",
+                newName: "isEnable");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "isEnable",
+                table: "SellerPaymentMethods",
+                newName: "isDisable");
+
+            migrationBuilder.RenameColumn(
+                name: "isEnable",
+                table: "SellerDeliveryOptions",
+                newName: "isDisable");
+        }
+    }
+}
